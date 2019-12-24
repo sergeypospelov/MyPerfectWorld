b202a28e0bd0e86029431e5f17a8f1f7a5950d52
sergeypospelov <sergeypospelov59@gmail.com>
2019-12-24 22:42:28 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/MyPerfectWorld.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyPerfectWorld.dir/main.cpp.o"
+  "MyPerfectWorld"
+  "MyPerfectWorld.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyPerfectWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
